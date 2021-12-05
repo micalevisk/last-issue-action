a634eced31cb09a21875b4c8f3e6e65e7140e9bc
Micael Levi (lab) <mllc@icomp.ufam.edu.br>
2021-12-05 18:02:37 -0400

    feat: using commonjs instead of ESM due to bundle issues

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,30 @@ import * as core from '@actions/core';
 
 import { findLastIssueWithLabels } from './find-last-issue-with-labels';
 
-if (process.env.NODE_ENV === 'development') await import('dotenv');
+async function main(): Promise<void> {
+  try {
+    const labels = core.getInput('labels', { required: true, trimWhitespace: true }).split(',');
 
-try {
-  const labels = core.getInput('labels', { required: true, trimWhitespace: true }).split(',');
+    const latestReportIssue = await findLastIssueWithLabels(labels);
 
-  const latestReportIssue = await findLastIssueWithLabels(labels);
+    let hasFoundSome = false;
 
-  let hasFoundSome = false;
+    if (latestReportIssue) {
+      hasFoundSome = true;
+      core.setOutput('issue_number', latestReportIssue.number);
+    }
 
-  if (latestReportIssue) {
-    hasFoundSome = true;
-    core.setOutput('issue_number', latestReportIssue.number);
+    core.setOutput('has_found', hasFoundSome);
+  } catch(err) {
+    if (err instanceof Error) {
+      core.error(err.message);
+    }
+
+    core.setFailed('Something went wrong!');
   }
+}
 
-  core.setOutput('has_found', hasFoundSome);
-} catch(err) {
-  if (err instanceof Error) {
-    core.error(err.message);
-  }
-
-  core.setFailed('Something went wrong!');
+// Check if is running as a script
+if (require.main === module) {
+  main();
 }
